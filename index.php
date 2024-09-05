913c3c9531e459d8f8531362216bf34cb64d7d4e
kyawphyowai94.github.io <kyawphyowai.sgg@gmail.com>
2024-09-05 21:26:18 +0630

    php lessons

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php 
     echo "Hello world!";
-    echo"<h1>Super Friday.</h1>"
+    echo"<h1>Super Friday.</h1>";
+    $language_type = "Server-side Programming Language";
+    echo $language_type;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>This is paragraph</h1>";
+    ?>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint expedita illo, eligendi, quis dolorum modi iste deserunt tenetur nostrum animi perferendis illum iure vitae dolores cupiditate explicabo doloremque quod.</p>
+
+    <script>
+        document.write('<h1>I am Javascript!</h1>');
+    </script>
+</body>
+</html>
---
+++ variable_and_string.php
@@ -0,0 +1,59 @@
+<?php
+// Variable
+// Naming Rule - No Space, No Special Characters(!@#$%^&*), Can't be a number on first character, Case Sensitive
+
+$name = "Kyaw Phyo Wai";
+$address = "Yangon";
+
+//Double quote and Single quote
+echo "My name is $name";
+echo '<br>';
+echo 'I live in $address';
+
+echo '<br>';
+echo 'I live in'.$address;
+echo '<br>';
+echo $name.' live in'.$address;
+
+ $first_name = 'Alan';
+ echo"<p>$first_name</p>";
+ $last_name = 'Walker';
+ echo"<p>$last_name</p>";
+
+ $first_name .= $last_name;
+ echo"<p>$first_name</p>";
+
+ //trim() => remove first space and last space
+ $singer = ' Taylor Swift ';
+ echo "<p>My favrious singer is$singer.</p>";
+ echo "<p>My favrious finger is".trim($singer).".</p>";
+
+ // strlen() => string length
+ echo strlen($singer);
+ echo "<br>";
+ $singer_length = trim($singer);
+ echo strlen($singer_length);
+ echo "<br>";
+
+
+ //strtolower(string) => change to lowercase
+ echo strtolower($singer);
+ echo "<br>";
+
+ //strtoupper(string) => change to uppercase
+ echo strtoupper($singer);
+ echo "<br>";
+
+ //substr(string,start(0), number) => substring
+ echo substr("Hello HeinLearner",6);
+ echo "<br>";
+ echo substr("Hello HeinLearner",6,4);
+ echo "<br>";
+ echo substr("Hello HeinLearner",10,5);
+ echo "<br>";
+
+ //str_replace(search, replace, subject)
+ $city = 'Hello Mandalay';
+ echo "<p>$city</p>";
+ echo str_replace('Mandalay','Smart City',$city);
+?>
